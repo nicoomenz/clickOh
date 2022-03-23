cd78f8104999ff77447cea9f93407642052b62f1
nicoomenz <nicoomenz@gmail.com>
2022-03-22 21:12:25 -0300

    settings

--- demo_backend/settings.py
+++ demo_backend/settings.py
@@ -166,7 +166,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = '/home/django/django_project/django_project/static'
+
 STATIC_URL = '/static/'
 
 # Default primary key field type
